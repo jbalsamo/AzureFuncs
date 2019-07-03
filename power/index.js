7a9be72616bcaae344aef1a9013e8adab957d935
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-07-03 11:04:28 -0400

    I got power

---
+++ power/index.js
@@ -0,0 +1,16 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.body.base && req.body.expo) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: { 'Answer': Math.pow(req.body.base,req.body.expo) }
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a base and exponent on the query string or in the request body"
+        };
+    }
+};
