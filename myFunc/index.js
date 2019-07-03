7cf4aeb1028bace04c016c8ddb3f46008b224482
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-07-03 10:46:56 -0400

    Change

--- myFunc/index.js
+++ myFunc/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello You Stinkin' Piece of " + (req.query.name || req.body.name)
+            body: "Hello You Stinkin' waste of " + (req.query.name || req.body.name)
         };
     }
     else {
