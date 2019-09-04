dcab95cede07cf0dbade7cce2b2b920ec6dfb8b0
rmoffitt <richard.a.moffitt@gmail.com>
2019-09-04 11:32:02 -0400

    made Rfunc

---
+++ Rfunc/index.js
@@ -0,0 +1,16 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Here we are, echoing the request: " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass something on the query string or in the request body"
+        };
+    }
+};
