06f1c532562063b159b38e25000a6684c16c8028
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-09-04 11:23:37 -0400

    Update

--- power/index.js
+++ power/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.body.base && req.body.expo) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: { 'This is the Answer': Math.pow(req.body.base,req.body.expo) }
+            body: { 'Power': Math.pow(req.body.base,req.body.expo) }
         };
     }
     else {
